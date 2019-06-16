919ab70600e78013fee28998d92a53a5929c28c5
lucas <lucas.matsales@gmail.com>
2019-06-16 18:17:21 -0300

    atualização basica

--- js/contasCtrl.js
+++ js/contasCtrl.js
@@ -1,17 +1,27 @@
 app.controller('contasCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.corConta = "";
-    $scope.corFisico = "";
-    $scope.neutro = "";
-    $scope.conta = {};
+    $scope.conta = {
+        cor:'',
+        banco:0,
+        fisico:0,
+        valor:0,
+        desc:"",
+    };
 
     $scope.contas = [];
 
     $scope.positivo = function(conta) {
-
+        conta.banco = parseFloat(conta.banco) + parseFloat(valor);
+        contas.push(conta);
+        conta.cor="bg-success"
     }
 
     $scope.negativo = function(conta) {
+        conta.banco = parseFloat(conta.banco) - parseFloat(valor);
+        contas.push(conta);
+        conta.cor="bg-danger"
+    }
 
+    $scope.delete = function(contas){
     }
 
 }]);
